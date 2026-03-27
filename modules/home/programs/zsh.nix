1f66e41ff54e529d5bc14c1339ffd52d68e11f56
Violet <me@violetshine.net>
2026-03-28 01:30:45 +0200

    programs: add nvm to zsh config

--- modules/home/programs/zsh.nix
+++ modules/home/programs/zsh.nix
@@ -1,4 +1,4 @@
-{
+{lib, ...}: {
   flake.modules.homeManager.violetshine = {
     programs.zsh = {
       enable = true;
@@ -8,6 +8,14 @@
         "always_to_end"
         "prompt_subst"
       ];
+
+      initContent = let
+        zshConfig = lib.mkOrder 1000 ''
+          export NVM_DIR="''${HOME}/.nvm"
+          [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+        '';
+      in
+        lib.mkMerge [zshConfig];
     };
   };
 }
