1e0d82522417a4c9e7e61a994428c0fd989138e8
Violet <me@violetshine.net>
2026-05-22 17:10:40 +0300

    programs(vscodium): add VS Codium

---
+++ modules/home/programs/desktop/vscodium.nix
@@ -0,0 +1,54 @@
+{
+  # VSCodium - a FOSS build of VS Code without Microsoft telemetry
+  flake.modules.homeManager.violetshine =
+    { pkgs, ... }:
+    {
+      programs.vscodium = {
+        enable = true;
+
+        # The VS Code package to use (VSCodium)
+        package = pkgs.vscodium;
+
+        profiles = {
+          default = {
+            enableUpdateCheck = false;
+            enableExtensionUpdateCheck = false;
+
+            extensions = [
+              # Automatically loads .envrc
+              pkgs.vscode-extensions.mkhl.direnv
+              # Nix support
+              pkgs.vscode-extensions.jnoortheen.nix-ide
+              # Tinymist - language server for Typst
+              pkgs.vscode-extensions.myriad-dreamin.tinymist
+              # Rust
+              pkgs.vscode-extensions.rust-lang.rust-analyzer
+              # Scala syntax highligting & LSP
+              pkgs.vscode-extensions.scala-lang.scala
+              pkgs.vscode-extensions.scalameta.metals
+            ];
+
+            userSettings = {
+              # Configure indentation
+              "editor.insertSpaces" = true;
+              "editor.tabSize" = 2;
+              "editor.indentSize" = "tabSize";
+
+              # Editor font
+              "editor.fontFamily" = "Lilex-Regular, monospace";
+              "editor.fontWeight" = "normal";
+              "editor.fontLigatures" = true;
+              "editor.fontSize" = 14;
+
+              # Nix language
+              "nix.enableLanguageServer" = true;
+              "nix.formatterPath" = "nixfmt";
+              "[nix]" = {
+                "editor.formatOnSave" = true;
+              };
+            };
+          };
+        };
+      };
+    };
+}
